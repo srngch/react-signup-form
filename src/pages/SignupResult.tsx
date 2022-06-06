4791b01923c47fad925c6f25aa77bcdd7579c153
srngch <srngch.dev@gmail.com>
2022-06-07 01:55:10 +0900

    feat: Add style to '/signup-result' page

--- src/pages/SignupResult.tsx
+++ src/pages/SignupResult.tsx
@@ -10,16 +10,19 @@ const SignupResult = ({ user }: { user: User | null }) => {
   }
 
   return (
-    <>
+    <div className='result-wrapper'>
       <h1>{user.username} 님 환영합니다!</h1>
       <div>
-        <p>회원가입이 완료되었습니다.</p>
+        <p>회원가입이 완료 되었습니다.</p>
         <p>
-          <span>{user.email}</span>
+          <span>지금부터 <code>{user.email}</code>로 로그인하여 서비스를 이용하실 수 있어요.</span>
         </p>
-        <Link to='/'>메인으로</Link>
+        <div className='button-wrapper'>
+          <Link className='button' to='/'>로그인하기</Link>
+          <Link className='button secondary' to='/'>메인으로</Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
